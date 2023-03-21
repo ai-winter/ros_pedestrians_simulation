47827be5e49e949f9870d2dca949ec6b3d5598e4
mr_winter <913982779@qq.com>
2023-03-21 19:20:51 +0800

    update: pedesrian rviz plugin

--- src/gazebo_sfm_plugin/scripts/main.sh
+++
@@ -1,3 +0,0 @@
-source ../../../devel/setup.bash
-python ./pedestrians_generate.py pedestrians_config.yaml
-roslaunch gazebo_sfm_plugin main.launch
---
+++ src/pedestrian_sim/ped_simulation/scripts/main.sh
@@ -0,0 +1,3 @@
+source ../../../../devel/setup.bash
+python ./pedestrians_generate.py pedestrians_config.yaml
+roslaunch gazebo_sfm_plugin main.launch
