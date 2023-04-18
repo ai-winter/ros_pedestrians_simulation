df87ead8705ab14afcbe192ab4bc3f44ee0f26d0
Guo Zhanyu <942568052@qq.com>
2023-04-18 14:24:46 +0800

    change file structure

--- src/ped_simulation/scripts/gap.sh
+++ src/ped_simulation/scripts/gap.sh
@@ -1,3 +1,3 @@
-source ../../../../devel/setup.bash
+source ../../../devel/setup.bash
 python ./pedestrians_generate.py pedestrians_config_gap.yaml
 roslaunch ped_simulation gap.launch
--- src/ped_simulation/scripts/main.sh
+++ src/ped_simulation/scripts/main.sh
@@ -1,3 +1,3 @@
-source ../../../../devel/setup.bash
+source ../../../devel/setup.bash
 python ./pedestrians_generate.py pedestrians_config.yaml
 roslaunch ped_simulation main.launch
